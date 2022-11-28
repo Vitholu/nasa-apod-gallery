02645cc67decb036df2c4221b556d56230cc29f6
Vitholu <116558222+Vitholu@users.noreply.github.com>
2022-11-28 10:35:11 +0100

    Add header section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Header from "./header/Header.jsx"
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+          <Header />
       </header>
     </div>
   );

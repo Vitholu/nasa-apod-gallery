6848b2bb5888290da70b6e44ebabed1726a96ff4
Vitholu <116558222+Vitholu@users.noreply.github.com>
2022-12-01 18:41:05 +0100

    Add fixed position to footer, and add asset folder

--- src/footer/Footer.jsx
+++ src/footer/Footer.jsx
@@ -1,11 +1,18 @@
 import "./footer.css"
+import ProfileIcon from "../assets/profile_icon.svg"
+import FavoriteIcon from "../assets/bookmark_icon.svg"
 
 import React from 'react'
 
 
 const Footer = () => {
   return (      
-    <div className="footer-bg"></div>
+    <div className="footer-bg">
+      <ul className="footer-nav">
+        <li><img src={FavoriteIcon} alt="" /></li>
+        <li><img className="profile-scale-icon" src={ProfileIcon} alt="" /></li>
+      </ul>
+    </div>
   )
 }
 
